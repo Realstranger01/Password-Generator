19554c0947178c2d159eecd90382327de8348d87
realstranet01 <117208687+Realstranger01@users.noreply.github.com>
2023-01-09 22:02:46 +0000

    update

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# Password-Generator
-create an application that an employee can use to generate a random password based on criteria they’ve selected by modifying starter code. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code that you write
+# Password Generator
+
+<h3 align="center">Password Generator</h3>
+
+## Getting Started:
+
+Simply open up the deployed link [Password Generator](https://realstranger01.github.io/Password-Generator/) and explore the website:
+
+![Password Generator](./assets/05-javascript-challenge-demo.png)
+
+### Prerequisites
+
+You need to have a modern browser able to handle html5 and css.
+
+Some popular browsers are:
+```
+
+Google Chrome
+Mozilla Firefox
+Safari
+Opera
+Edge
+```
+### Description
+
+I created this password generator to display my JavaScript skills. This website, when the user clicks the "Generate Password" button will display a series of prompts to the user. The user can select the length of the password between 8 and 128 characters and can choose characters of uppercase, lowercase, numbers or symbols.
+
+### Installation
+
+The intallation process consisted of:
+```
+
+* Application deployed at live URL.
+
+* Application loads with no errors.
+
+* GitHub repository contains application code.
+```
+
+### What I Learned
+
+I learned many important fundementals of javascript creating this project. I used a combination of objects, arrays, function calls, while statements and if statements. I learned how to call functions, return values and many different manipulations that can be performed on objects, arrays and numbers like Math.Floor, Math.random. I also learned commonly used operators like ! and ===. I really enjoyed my time learning and creating this project!
+
+## Built With
+
+* Html
+
+* CSS
+
+* Javascript
+
+## licence
+
+Please refer to the licence in the repo.
+
+## Author
+
+* Hamza Osman - Intial work - [RealStranger](https://github.com/Realstranger01/Password-Generator.git)
+
+
---
+++ starter/script.js
@@ -0,0 +1,186 @@
+
+
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.'
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z'
+];
+
+// Function to prompt user for password options
+function getPasswordOptions() {
+  var pwdLength = prompt("How long do you want your password to be?");
+  //Validate the number provided by the user. 10-64 
+  if (pwdLength >= 10 && pwdLength <= 64) {
+    var includeLowerCase = confirm("Do you want your password to include lower case letters?");
+    var includeUpperCase = confirm("Do you want your password to include upper case letters");
+    var includeSpecialCharacters = confirm("Do you want your password to include special characters?");
+    var includeNumbers = confirm("Do you want your password to include numbers?");
+
+    return {
+      pwdLength: pwdLength,
+      includeLowerCase: includeLowerCase,
+      includeUpperCase: includeUpperCase,
+      includeSpecialCharacters: includeSpecialCharacters,
+      includeNumbers: includeNumbers,
+    }
+  }
+  else {
+    alert("Password must be between 10 and 64 characters");
+  }
+}
+
+// Function for getting a random element from an array
+function getRandom(arr) {
+  return item = arr[Math.floor(Math.random() * arr.length)];
+}
+
+// Function to generate password with user input
+function generatePassword() {
+
+  var passwordOptions = getPasswordOptions();
+  console.log(passwordOptions);
+
+  // build a new array based on the passwordOptions variable
+
+
+
+  //Build a string of password based on the users options and from the provided characters.
+  var generatedPassword = "";
+
+  if (passwordOptions.includeLowerCase == false && passwordOptions.includeUpperCase == false && passwordOptions.includeSpecialCharacters == false && passwordOptions.includeNumbers == false) {
+    alert("Atleast one character type should be selected..!");
+
+  } 
+  else {
+    while (generatedPassword.length != passwordOptions.pwdLength) {
+      //lowercase
+      if (passwordOptions.includeLowerCase == true && generatedPassword.length != passwordOptions.pwdLength) {
+        generatedPassword += getRandom(lowerCasedCharacters);
+      }
+
+      //uppercase
+      if (passwordOptions.includeUpperCase == true && generatedPassword.length != passwordOptions.pwdLength) {
+        generatedPassword += getRandom(upperCasedCharacters);
+      }
+
+      //special characters
+      if (passwordOptions.includeSpecialCharacters == true && generatedPassword.length != passwordOptions.pwdLength) {
+        generatedPassword += getRandom(specialCharacters);
+      }
+
+      //numbers
+      if (passwordOptions.includeNumbers == true && generatedPassword.length != passwordOptions.pwdLength) {
+        generatedPassword += getRandom(numericCharacters);
+      }
+    }
+  }
+
+
+
+  return generatedPassword;
+
+  // create a loop that will iterate based on passwordOptions.pwdLength
+
+  //       // for each loop iteration invoke getRandom to get a random character from the new passwordOptions array
+  //     }
+  //   }
+  // }
+}
+
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector('#password');
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
